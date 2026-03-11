28a9a156ad7c794a728317424b15b67c068d0b33
mdissaqh <mdissaqh2007@gmail.com>
2026-03-11 16:22:09 +0530

    Hands on with socket io

---
+++ day18/Backend/server.js
@@ -0,0 +1,23 @@
+import app from "./src/app.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+io.on("connection", (socket) => {
+    console.log("Hello guys, we are now updating our server.") 
+    socket.on("message",(msg)=>{
+        console.log(msg)
+        io.emit("abc",msg)
+    })
+});
+
+
+
+
+
+httpServer.listen(3000,()=>{
+    console.log("Server is running on port 3000")
+})
