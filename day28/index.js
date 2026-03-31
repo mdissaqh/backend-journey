e7b2f85dae37608f2a05c1eb10855d288b449377
mdissaqh <mdissaqh2007@gmail.com>
2026-03-31 19:33:22 +0530

    On Day 28 did Hands on with Type Script

---
+++ day28/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function bye(name) {
+    console.log(`Bye ${name}`);
+}
+bye("Issaqh");
---
+++ day28/index.ts
@@ -0,0 +1,5 @@
+function bye(name:string):void{
+    console.log(`Bye ${name}`)
+}
+
+bye("Issaqh")
